10ccaa553792bc944667dc8a8e3c5aafcd14f007
Paul Schwarzenberger <paul@celidor.net>
2018-07-29 08:29:08 +0100

    added Jenkins server public IP as Terraform output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "jenkins_url" {
   value = "https://jenkins-${terraform.workspace}.${var.dns_zone}"
 }
+
+output "jenkins_server_public_ip" {
+  value = "${module.jenkins-master.public_ip}"
+}
